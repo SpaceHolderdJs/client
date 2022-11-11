89aa31147581e5f40df2844c3a679c6adf49a98d
Igor <sergienko339@gmail.com>
2022-11-11 12:01:26 +0200

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Layout from "antd/lib/layout";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/routes/routes";
+import { UserContexProvider } from "./contexts/user";
+import { NotebookContexProvider } from "./contexts/notebooks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}>
+      <UserContexProvider>
+        <NotebookContexProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </NotebookContexProvider>
+      </UserContexProvider>
+    </Layout>
   );
 }
 
